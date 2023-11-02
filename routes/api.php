5d1eb44dec98f534682ec8747de340a8605b79f0
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-11-03 04:01:26 +0600

    jwt auth signup, login, logout refresh, payload

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{ClasssController, SectionController, StudentController};
+use App\Http\Controllers\{ClasssController, SectionController, StudentController, AuthController};
 use Illuminate\Support\Facades\Route;
 
 //__class api route__//
@@ -13,3 +13,17 @@ Route::apiResource("/section", SectionController::class);
 
 //__student api route__//
 Route::apiResource("/student", StudentController::class);
+
+
+Route::group([
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post("payload", [AuthController::class, 'payload']);
+    Route::post("register", [AuthController::class, "register"]);
+});
